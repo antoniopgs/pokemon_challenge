7e284dd5f9d7bc458832e97213e5a5168ebec39d
António <apgoncalves@outlook.pt>
2020-06-03 15:35:39 +0100

    Add more stuff

--- pminds_challenge.py
+++ pminds_challenge.py
@@ -6,15 +6,14 @@ while True:
     """ If the set of the input string has length of 1, it means all chars are equal.
     If all chars are equal, the number of caught Pokemon will equal the string's length + 1 (for the starting position)."""
     if len(set(movement_input)) == 1:
-        print(len(movement_input) + 1)
-        
+        print(len(movement_input) + 1)    
     else:
         # The coordinate pair for Ash's current position (he begins at 0, 0):
         ash_x, ash_y = 0, 0
 
         """ The number of Caught Pokemons will equal the number of UNIQUE Passed Positions (that's why a set is used).
-        (0, 0) is already registered because Ash always catches his 1st Pokemon at his starting position.
-        (I used tuples because the coordinates of each position should be ordered and immutable)."""
+        The position (0, 0) is already registered because Ash always catches his 1st Pokemon at the beggining.
+        I used tuples because the coordinates of each position should be ordered and immutable."""
         passed_positions = {(0, 0)}
 
         for char in movement_input:
